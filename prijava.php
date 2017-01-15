d30d6172b19daa3eb085f308d5d1f2fce816b10e
oljubuncic1 <oljubuncic1@etf.unsa.ba>
2017-01-15 02:17:24 +0100

    Pocetni dizajn forme za prijave

---
+++ prijava.php
@@ -0,0 +1,187 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width,initial-scale=1">
+
+    <title>Soft Skills Academy Sarajevo '17</title>
+
+     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js">
+    </script>
+
+
+    <link rel="stylesheet" href="bootstrap/css/bootstrap-theme.min.css">
+    <link rel="stylesheet" href="bootstrap/css/bootstrap-theme.css">
+    <link rel="stylesheet" href="bootstrap/css/bootstrap.css">
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/kontakt.css">
+    <link rel="stylesheet" href="css/prijava.css">
+
+     <script src="bootstrap/js/bootstrap.min.js"></script>
+    <script src="bootstrap/js/bootstrap.js"></script>
+    <script src="js/prijava.js"></script>
+
+</head>
+
+<body>
+
+<?php include 'partials/mainNavbar2.html'; ?>
+
+
+    <div class="container">
+        <div class="row">
+            <div class="col-xs-12">
+                <p class="naslov">Prijavi se - Budi korak ispred!</p>
+            </div>
+        </div>
+        <div class="row form-group">
+            <div class="col-xs-12 col-md-4">
+                <ul class="nav nav-pills nav-stacked  thumbnail setup-panel koraci">
+                    <li class="active"><a class="korak" href="#step-1">
+                        <h4 class="list-group-item-heading tekstKoraka">Korak 1</h4>
+                        <p class="list-group-item-text tekstKoraka">Osnovni podaci</p>
+                    </a></li>
+                    <li ><a class="korak" href="#step-2">
+                        <h4 class="list-group-item-heading tekstKoraka">Korak 2</h4>
+                        <p class="list-group-item-text tekstKoraka">Podaci o obrazovanju</p>
+                    </a></li>
+                    <li ><a class="korak" href="#step-3">
+                        <h4 class="list-group-item-heading tekstKoraka">Korak 3</h4>
+                        <p class="list-group-item-text tekstKoraka">Motivaciono pismo</p>
+                    </a></li>
+                    <li><a class="korak" href="#step-4">
+                        <h4 class="list-group-item-heading tekstKoraka">Korak 4</h4>
+                        <p class="list-group-item-text tekstKoraka">Prethodno iskustvo</p>
+                    </a></li> 
+                    <li ><a class="korak" href="#step-5">
+                        <h4 class="list-group-item-heading tekstKoraka">Korak 5</h4>
+                        <p class="list-group-item-text tekstKoraka">Ostalo</p>
+                    </a></li>   
+                </ul>
+            </div>
+            <div class="col-xs-12 col-md-8">
+            <form>
+
+    <div class="setup-content" id="step-1">
+            <div class="col-xs-12 well text-center">
+                <h1> STEP 1</h1>
+
+<!-- <form> -->               
+                
+    
+        <a id="prethodni" class="btn pull-left sljedeci">Prethodni korak</a><a id='sljedeci' class="btn prethodni pull-right">Sljedeći korak</a>
+    
+                
+<!-- </form> -->
+                
+            </div>
+        
+    </div>
+
+</form>
+
+
+
+            <form>
+
+    <div  class="setup-content" id="step-2">
+            <div class="col-xs-12 well text-center">
+                <h1> STEP 2</h1>
+
+<!-- <form> -->               
+                
+    
+        <a id="prethodni" class="btn pull-left sljedeci">Prethodni korak</a><a id='sljedeci' class="btn prethodni pull-right">Sljedeći korak</a>
+    
+                
+<!-- </form> -->
+                
+            </div>
+        
+    </div>
+
+</form>
+
+
+
+            <form>
+
+    <div  class="setup-content" id="step-3">
+            <div class="col-xs-12 well text-center">
+                <h1> STEP 3</h1>
+
+<!-- <form> -->               
+                
+    
+        <a id="prethodni" class="btn pull-left sljedeci">Prethodni korak</a><a id='sljedeci' class="btn prethodni pull-right">Sljedeći korak</a>
+    
+                
+<!-- </form> -->
+                
+            </div>
+        
+    </div>
+
+</form>
+
+
+
+            <form>
+
+    <div  class="setup-content" id="step-4">
+            <div class="col-xs-12 well text-center">
+                <h1> STEP 4</h1>
+
+<!-- <form> -->               
+                
+    
+        <a id="prethodni" class="btn pull-left sljedeci">Prethodni korak</a><a id='sljedeci' class="btn prethodni pull-right">Sljedeći korak</a>
+    
+                
+<!-- </form> -->
+                
+            </div>
+        
+    </div>
+
+</form>
+
+
+
+            <form>
+
+    <div  class="setup-content" id="step-5">
+            <div class="col-xs-12 well text-center">
+                <h1> STEP 5</h1>
+
+<!-- <form> -->               
+                
+   
+        
+        <a id="prethodni" class="btn pull-left sljedeci">Prethodni korak</a><a id='sljedeci' class="btn prethodni pull-right">Sljedeći korak</a>
+    
+                
+<!-- </form> -->
+                
+            </div>
+        
+    </div>
+
+</form>
+
+</div>
+        </div>
+    </div>  
+
+
+
+    
+
+
+<?php include 'partials/footer.html'; ?>
+
+
+</body>
+
+</html>
