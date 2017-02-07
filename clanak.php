ffd3d589dab5ccbc4eb1a24819b76d4b0ea87781
oljubuncic1 <oljubuncic1@etf.unsa.ba>
2017-02-07 21:58:33 +0100

    refactored

---
+++ clanak.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    
+    <?php include 'partials/headerExt.html'; ?>
+    <link rel="stylesheet" href="css/clanak.css">
+
+</head>
+
+<body>
+
+<?php include 'partials/mainNavbar2.html'; ?>
+
+<!-- kod clanak -->
+
+
+<?php include 'partials/footer.html'; ?>
+
+
+</body>
+
+</html>
---
+++ treninzi.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    
+    <?php include 'partials/headerExt.html'; ?>
+    <link rel="stylesheet" href="css/treninzi.css">
+
+</head>
+
+<body>
+
+<?php include 'partials/mainNavbar2.html'; ?>
+
+<!-- kod treninzi -->
+
+
+<?php include 'partials/footer.html'; ?>
+
+
+</body>
+
+</html>
