7974c3513f7a29bc0d636c2113aac9e6fa1df6f0
Faris Odobasic <Faris Odobasic>
2017-02-07 22:12:22 +0100

    galerija start

---
+++ galerija.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    
+    <?php include 'partials/headerExt.html'; ?>
+    <link rel="stylesheet" href="css/galerija.css">
+
+</head>
+
+<body>
+
+<?php include 'partials/mainNavbar2.html'; ?>
+
+<div class="marginContainer">
+    <div class="container-fluid">
+    	<h1>Heheeee</h1>    
+    </div>
+
+</div>
+
+
+<?php include 'partials/footer.html'; ?>
+
+
+</body>
+
+</html>
